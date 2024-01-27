3d6334d78863cd2bf6cbc29e3646b9d04194d0b2
Owen <heewon.dev@gmail.com>
2024-01-27 22:15:52 +0900

    fix: font lib (#2)

---
+++ packages/utils-next-font/pretendard/src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var r = require('next/font/local');
+
+function _interopDefault(e) {
+  return e && e.__esModule ? e : { default: e };
+}
+
+var r__default = /*#__PURE__*/ _interopDefault(r);
+
+const a = r__default.default({ src: '../PretendardVariable.woff2', display: 'swap' });
+
+exports.pretendardFont = a;
